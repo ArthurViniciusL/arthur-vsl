f3efc370551525bd7daf2eba4ac27919c16ca009
arthurviniciusl <arthurviniciussl.contato@gmail.com>
2024-07-10 12:44:53 -0300

    project created

---
+++ arthur-vsl-app/src/app/linux/conf-next-project.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#!/bin/bash
+
+# Autor: Arthur Vinicius Souza Lucena - 10/07/2024
+# version 1.0
+
+echo "Arthur.vsl"
+
+echo "Iniciando..."
+
+cd src/app
+
+echo "Criandos pastas..."
+
+foldersPattern=("components" "views" "styles" "assets/images" "utils" "api" "tests" "json" "linux" );
+
+for foldersPattern in "${foldersPattern[@]}"
+do
+	mkdir $foldersPattern
+done
+
+echo "Deletando arquivos..."
+
+reactDefaults=("layout.tsx" "globals.css" "page.module.css");
+
+for reactDefaults in "${reactDefaults[@]}"
+do
+	rm $reactDefaults
+done
+
+cd styles
+touch style.css
+
+cd ../../
+
+echo "Conluido!"
