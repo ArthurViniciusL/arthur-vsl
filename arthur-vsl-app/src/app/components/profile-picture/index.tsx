11b110dba1f3ad702203ba6ed31bb6b99b20e73a
arthurviniciusl <arthurvinicius.contato@gmail.com>
2024-07-11 16:06:38 -0300

    Created the initial components, along with the scroll-snap-type

---
+++ arthur-vsl-app/src/app/components/profile-picture/index.tsx
@@ -0,0 +1,30 @@
+import styles from './ProfilePicture.module.css'
+
+import Image from "next/image";
+
+import imgProfile from "../../assets/images/img_profile.jpg";
+
+/* interface ViewSizeProps {
+    size: number;
+}
+interface BorderColor {
+    color?: string
+} */
+
+
+export default function ProfilePicture() {
+    /* const borderColor = {
+        border: `2px solid ${color}`
+     } */
+    return (
+        <picture>
+            <Image
+                className={styles.imgStyle}
+                src={imgProfile}
+                alt="imagem_do_perfil"
+                priority={true}
+            />
+        </picture>
+    )
+}
+
