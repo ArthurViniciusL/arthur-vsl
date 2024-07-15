b8a5e4c5efacc891aebdeb930001f864dd7b2f65
arthurviniciusl <arthurvinicius.contato@gmail.com>
2024-07-15 18:40:51 -0300

    Implemented the logic for changing the theme

---
+++ arthur-vsl-app/src/app/pages/Home/pages.tsx
@@ -0,0 +1,14 @@
+
+import styles from "./Home.module.css";
+import ProfilePicture from "../../components/profile-picture";
+import { Header } from "../../components/Header";
+import { ListSocialNetwork } from "../../components/SocialNetwork";
+
+
+
+/* Home */
+export default function Home() {
+  return (
+    <h1>Olá</h1>
+  );
+}
