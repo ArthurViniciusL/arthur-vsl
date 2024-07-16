8f208ca8b8b71f4908d794f1e60841c995aa5597
arthurviniciusl <arthurvinicius.contato@gmail.com>
2024-07-16 16:25:47 -0300

    Responsiveness correction made in the first section

---
+++ arthur-vsl-app/src/app/components/Home-Components/AboutMe/index.tsx
@@ -0,0 +1,19 @@
+import styles from './AboutMe.module.css';
+
+export function AboutMe() {
+    return (
+        <div className={styles.Description}>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore recusandae ipsum vitae odit omnis eos beatae illum, magnam magni iusto optio eveniet, tempora quibusdam officiis ducimus quia adipisci corporis impedit!</p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque quae ipsum molestias architecto odit accusamus dolor vero amet, incidunt aspernatur, natus magni tempora neque. Debitis temporibus error nesciunt modi provident!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio autem ad minima atque. Voluptatibus assumenda tenetur, molestiae quaerat, minus incidunt, placeat doloremque voluptatum laboriosam dolorum maiores et earum commodi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio autem ad minima atque. Voluptatibus assumenda tenetur, molestiae quaerat, minus incidunt, placeat doloremque voluptatum laboriosam dolorum maiores et earum commodi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio autem ad minima atque. Voluptatibus assumenda tenetur, molestiae quaerat, minus incidunt, placeat doloremque voluptatum laboriosam dolorum maiores et earum commodi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio autem ad minima atque. Voluptatibus assumenda tenetur, molestiae quaerat, minus incidunt, placeat doloremque voluptatum laboriosam dolorum maiores et earum commodi.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio autem ad minima atque. Voluptatibus assumenda tenetur, molestiae quaerat, minus incidunt, placeat doloremque voluptatum laboriosam dolorum maiores et earum commodi.
+            </p>
+        </div>
+    )
+}
