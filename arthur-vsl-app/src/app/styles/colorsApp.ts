c9a237d5001d9ebe4570a6cb964a2cab0dbf43b6
arthurviniciusl <arthurvinicius.contato@gmail.com>
2024-07-17 10:26:50 -0300

    Created the component to use the Human interface guidelines

---
+++ arthur-vsl-app/src/app/styles/colorsApp.ts
@@ -0,0 +1,26 @@
+const UiGuidelines = {
+    "ligth": {
+        "bg-color-01": "#ffffff",
+        "bg-color-02": "#f8f9fb",
+        "bg-color-03": "#f6f5f4",
+        "bg-color-04": "#deddda",
+        "font-color-01": "#000000",
+        "font-color-02": "#241f31"
+    },
+    "dark": {
+        "bg-color-01": "#131416",
+        "bg-color-02": "#1a1c1e",
+        "bg-color-03": "##241f31",
+        "bg-color-04": "#000000",
+        "font-color-01": "#ffffff",
+        "font-color-02": "#f6f5f4"
+    },
+    "yellow": "#ffbd08",
+    "blue": "174AE4",
+    "confirm": "#57e389",
+    "warning": "#f60000",
+    "btn-border-radius": "15px",
+    "menu-border-radius": '20px',
+};
+
+export default UiGuidelines;
