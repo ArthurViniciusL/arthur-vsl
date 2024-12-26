350b6a8a2d07db3bcc06aab174add982d7a94d96
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2024-12-26 20:52:20 -0300

    added custom.s.ts and eslint.config.mjs

---
+++ custom.d.ts
@@ -0,0 +1,38 @@
+/* @arthur.vsl */
+
+// Declarações necessárias para outros tipos de mídia
+
+/* declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.webm' {
+    const src: string;
+    export default src;
+} */
+
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.webm' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.ogg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.avi' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.mov' {
+    const src: string;
+    export default src;
+}
