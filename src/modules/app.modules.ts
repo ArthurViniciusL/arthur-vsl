2f02e1c40df2dd0f63fc1fa342b6bdfbc0eb4e1c
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2024-12-26 20:57:35 -0300

    added /modules

---
+++ src/modules/app.modules.ts
@@ -0,0 +1,13 @@
+/* 
+* This file is an implementation inspired by the way
+* Angular organizes the modularization of components in
+* its projects
+*/
+
+export {
+    Github       as GithubIcon,
+    Linkedin     as LinkedinIcon,
+    Mail         as MailIcon,
+    Instagram    as InstagramIcon,
+    ExternalLink as ExternalLinkIcon
+} from "lucide-react";
