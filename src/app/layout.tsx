cf992c917885344fa84bad380b48263b0ad58aaf
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2024-12-26 21:04:52 -0300

    added import alias and video on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "@/styles/globals.css";
+import type { Metadata } from "next";
 import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
