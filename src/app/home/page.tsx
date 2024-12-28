77ec5a4a57ea6bab029911952c604edabcac0fdc
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2024-12-27 22:00:54 -0300

    Main page cards created

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import { ImageProfile } from "@/components/ImageProfile";
 import styles from "./home.module.css"
 import { SocialMedia } from "@/components/SocialMedia";
+import { CardProject } from "@/components/CardProject";
 
 export default function Home() {
     return (
@@ -8,9 +9,9 @@ export default function Home() {
             <section className={`${styles.content}`}>
                 <ImageProfile />
                 <div className={`${styles.textArea}`}>
-                    <h1 className="art:font-size:title">
+                    <h2 className="art:font-size:subtitle-01">
                         Sobre mim
-                    </h1>
+                    </h2>
                     <p className="art:font-size:normal">
                         Olá! Meu nome é Arthur, sou desenvolvedor web full-stack, um cinéfilo nas horas vagas e paraibano de coração.
                     </p>
@@ -21,9 +22,20 @@ export default function Home() {
                     <p className="art:font-size:normal">
                         Durante minha jornada na universidade, tive a oportunidade de desempenhar múltiplos papéis, desde desenvolvedor front-end até designer de UI/UX, Scrum Master e QA. Essas experiências não apenas fortaleceram minhas habilidades técnicas, mas também me permitiram explorar diferentes aspectos do desenvolvimento de software.
                     </p>
-                    
+
                     <SocialMedia />
                 </div>
+            </section>
+
+            <section className={`${styles.content} ${styles.contentProject}`}>
+                <h1 className="art:font-size:title">
+                    Meu projetos
+                </h1>
+                <CardProject />
+
+                <button>
+                    Veja main
+                </button>
             </section>
         </main>
     );
