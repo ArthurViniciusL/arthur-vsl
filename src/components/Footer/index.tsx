b5ca08fd921f71e3b8af173ef0e9c49533bacf6d
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2024-12-27 23:26:11 -0300

    Footer component created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./footer.module.css"
+
+export function Footer() {
+    return (
+        <>
+            <footer className={`${styles.footer} art:bg:white-03`}>
+                <div className={styles.content}>
+                    <p className="art:font-size:normal-01">
+                        © 2025 Arthur Lucena. Todos os direitos reservados.
+                    </p>
+                </div>
+            </footer>
+        </>
+    )
+}
