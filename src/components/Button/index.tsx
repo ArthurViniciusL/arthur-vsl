290f0c119aefa1d588ed4f9b1ffe7a335ef85661
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2024-12-31 20:45:49 -0300

    Changing component style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,5 @@
+import styles from "./button.module.css";
+
 interface ButtonProps {
     onClick?: () => void;
     className?: string;
@@ -10,15 +12,17 @@ export function Button({ onClick, className, children }: ButtonProps) {
             <button 
                 onClick={onClick}
                 className={`
-                    ${className}
+                    ${styles.button}
                     art:cursor:pointer
                     art:p-01
                     art:border-radius:r-01
                     art:font-size:normal-01
                     art:font-color:black-01
-                    art:hover:font-color:white-02
                     art:bg:white-02
+                    
+                    art:bg:font-color:white-01
                     art:hover:bg:black-01
+                    ${className}
                 `}>
                 {children}
             </button>
