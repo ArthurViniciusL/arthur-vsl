dc87a41cb8d9ea086b38a0329630d00d5af489e7
Arthur Vinicius <arthur@debian>
2025-03-16 10:48:36 -0300

    Changes to be committed: 	modified:   src/styles/art-css

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1ef9ea383188a504")
+/******/ 		__webpack_require__.h = () => ("509d7d5d06f9d2cc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -23,17 +23,17 @@ export default function Home() {
                 <section className={`${styles.content}`}>
                     <ImageProfile />
                     <div className={`${styles.textArea}`}>
-                        <h2 className="art:font:subtitle-01">
+                        <h2 className="art:font:3xl">
                             Sobre mim
                         </h2>
-                        <p className="art:font:normal-02">
+                        <p className="art:font:base">
                             Olá! Meu nome é Arthur, sou desenvolvedor web full-stack, um cinéfilo nas horas vagas e paraibano de coração.
                         </p>
-                        <p className="art:font:normal-02">
+                        <p className="art:font:base">
                             Atualmente, estou no sétimo período da graduação em Sistemas de Informação, onde descobri minha paixão por criar e aprimorar produtos por meio de soluções criativas. Nos últimos anos, acumulei experiência no desenvolvimento de software ao me envolver em diversos projetos acadêmicos.
                         </p>
 
-                        <p className="art:font:normal-02">
+                        <p className="art:font:base">
                             Durante minha jornada na universidade, tive a oportunidade de desempenhar múltiplos papéis, desde desenvolvedor front-end até designer de UI/UX, Scrum Master e QA. Essas experiências não apenas fortaleceram minhas habilidades técnicas, mas também me permitiram explorar diferentes aspectos do desenvolvimento de software.
                         </p>
 
