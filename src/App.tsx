301b8d254cf932a8da919c9320f645f24d6535f5
arthurviniciussl <arthurviniciussl.contato@gmail.com>
2025-09-30 01:37:49 -0300

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import "./styles/global.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="bg-art-secondary">
+      <main className="">
         <h1>Vite + React</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-      </div>
+        </Button>
+        
+      </main>
     </>
   );
 }
