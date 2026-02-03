0dc4c5cecb9f94283f0c69dd4adf9525d8211d87
arthurviniciussl <arthurviniciussl.contato@gmail.com>
2026-02-03 15:57:48 -0300

    red border removed

--- src/components/layout/main/index.tsx
+++ src/components/layout/main/index.tsx
@@ -5,7 +5,7 @@ interface IMainProps extends React.ComponentProps<"main"> { }
 export default function Main({ className, children, ...props }: IMainProps) {
     return (
         <>
-            <main className={cn("w-full md:w-7xl p-4 flex flex-col items-center border border-red-500", className)} {...props}>
+            <main className={cn("w-full md:w-7xl p-4 flex flex-col items-center", className)} {...props}>
                 {children}
             </main>
         </>
