30b2b11476cff8c2f9861095c71261d74ea12452
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2026-02-06 21:43:08 -0300

    Remove absolute positioning from button

--- src/components/app/home/SpeakAboutMe/SpeakAboutMe.tsx
+++ src/components/app/home/SpeakAboutMe/SpeakAboutMe.tsx
@@ -5,7 +5,7 @@ export default function SpeakAboutMe() {
     return (
         <>
             <TooltipApp side='right' msg='Me ouvir'>
-                <button className="cursor-pointer bg-blue-500/40 z-10 hover:bg-blue-500/50 backdrop-blur-md border border-blue-500 absolute right-0 mt-2 text-blue-500 rounded-full flex items-center justify-center p-2">
+                <button className="cursor-pointer bg-blue-500/40 z-10 hover:bg-blue-500/50 backdrop-blur-md border border-blue-500 right-0 mt-2 text-blue-500 rounded-full flex items-center justify-center p-2">
                     <IcVolume2 size={18}/>
                 </button>
             </TooltipApp>
